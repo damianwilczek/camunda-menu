1b67b3df6a31b36b3e6769700bf0b452f726f113
Damian <damian@telenetik.pl>
2016-02-24 17:31:04 +0100

    another changes

--- app/GiveMeSomeFood/platforms/android/assets/www/js/index.js
+++ app/GiveMeSomeFood/platforms/android/assets/www/js/index.js
@@ -38,6 +38,7 @@ var app = {
             type: "GET", url: "https://www.system-solution.it/test.php", success: function (msg) {
                 if (msg != 'error') {
                     var data = JSON.parse(msg);
+                    localStorage.removeItem("data");
                     localStorage.setItem("data", JSON.stringify(data));
                 }
             }
@@ -64,51 +65,16 @@ var app = {
             }
         });
     },
-    convertImg: function() {
-        $('img.svg').each(function(){
-            var $img = jQuery(this);
-            var imgID = $img.attr('id');
-            var imgClass = $img.attr('class');
-            var imgURL = $img.attr('src');
-
-            $.get(imgURL, function(data) {
-                // Get the SVG tag, ignore the rest
-                var $svg = jQuery(data).find('svg');
-
-                // Add replaced image's ID to the new SVG
-                if(typeof imgID !== 'undefined') {
-                    $svg = $svg.attr('id', imgID);
-                }
-                // Add replaced image's classes to the new SVG
-                if(typeof imgClass !== 'undefined') {
-                    $svg = $svg.attr('class', imgClass+' replaced-svg');
-                }
-
-                // Remove any invalid XML tags as per http://validator.w3.org
-                $svg = $svg.removeAttr('xmlns:a');
-
-                // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
-                if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-                    $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-                }
-
-                // Replace image with new SVG
-                $img.replaceWith($svg);
-
-            }, 'xml');
-        });
-    },
     getFood: function() {
         localStorage.removeItem('selectedFood');
         var data = JSON.parse(localStorage.getItem("data"));
         var HTML = '';
         $.each(data.Food, function( key, value) {
             if(value.location == data.currentLocationId) {
-                HTML += ' <tr><td class="left"><label id="foodID_'+ value.id +'" onclick="app.selectFood(this.id)"><img class="svg" src="res/bold-check-button.svg" /></label></td><td class="right">' + value.name + '</td></tr>';
+                HTML += ' <tr onclick="app.selectFood(this.firstElementChild.lastElementChild.id)"><td class="left"><label id="foodID_'+ value.id +'"><img src="res/check-default.png" /></label></td><td class="right">' + value.name + '</td></tr>';
             }
         });
         document.getElementById('foodList').innerHTML = HTML;
-        app.convertImg();
     },
     selectFood: function (id) {
        if (localStorage.getItem("selectedFood") !== null) {
@@ -119,31 +85,32 @@ var app = {
 
             var oldElement = document.getElementById(foodSelected);
             oldElement.parentElement.style.backgroundColor = '';
-            oldElement.lastElementChild.firstElementChild.firstElementChild.firstElementChild.style.fill = '';
+            oldElement.lastElementChild.setAttribute('src', "res/check-default.png");
             oldElement.parentElement.parentElement.lastElementChild.style.backgroundColor = '';
 
        }
 
         var newElement = document.getElementById(id);
-        var button = document.getElementById('orderFood')
+        var button = document.getElementById('orderFood');
 
         newElement.parentElement.style.backgroundColor = '#1ea184';
-        newElement.lastElementChild.firstElementChild.firstElementChild.firstElementChild.style.fill = '#ffffff';
+        newElement.lastElementChild.setAttribute('src', "res/check-active.png");
         newElement.parentElement.parentElement.lastElementChild.style.backgroundColor = '#e9f6f3';
 
         button.style.backgroundColor = '#1ea184';
         button.disabled = false
         button.value = id;
 
-        $("#buttonDiv").css('top', eval($('#'+id).offset().top - 410));
+        $("#buttonDiv").css('top', eval($('#'+id).offset().top - 234));
 
         localStorage.setItem('selectedFood', id);
     },
     removeSelected: function(){
         var button = document.getElementById('orderFood');
         var oldElement = document.getElementById(button.value);
+
         oldElement.parentElement.style.backgroundColor = '';
-        oldElement.lastElementChild.firstElementChild.firstElementChild.firstElementChild.style.fill = '';
+        oldElement.lastElementChild.setAttribute('src', "res/check-default.png");
         oldElement.parentElement.parentElement.lastElementChild.style.backgroundColor = '';
         button.style.backgroundColor = '';
         button.disabled = true
